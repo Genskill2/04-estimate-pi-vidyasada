4920833ce38d7cf7858fcc3f683b54d8da31d024
vidyasada <84785618+vidyasada@users.noreply.github.com>
2021-06-04 13:19:36 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,19 @@ int main(void) {
     }
   }
 }
-
-
-
+float mc_pi(int count)
+{
+ float circle=0;
+ for(int i=0;i<count;i++)
+ {
+  
+  float x = frandom();
+  float y = frandom();
+  if(sqrt(x*x+y*y)<=1)
+  {
+   circle++;
+  }
+ }
+ float area =(float) (4*circle)/(count);
+ return area;
+}
